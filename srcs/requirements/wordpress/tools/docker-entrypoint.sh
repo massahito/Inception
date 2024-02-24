2763563243766581482f1009f9a395c00ff2aba2
marai <masadevs@proton.me>
2024-02-24 11:24:34 +0000

    change wordpress user id

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -eux
+groupmod -g ${GROUP_ID} -o wordpress
+usermod -u ${USER_ID} -g wordpress -o wordpress
+chown -R wordpress:wordpress /run/php /var/www/html /var/www/wordpress /docker /var/log/
 if [ ! -e /var/www/html/wp-config.php ]; then
 	cd /var/www/html
 	cp -fr /var/www/wordpress/* /var/www/html/
-	wp config create --allow-root --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbhost="${WORDPRESS_DB_HOST}" --dbpass="${WORDPRESS_DB_PASSWORD}" --path="/var/www/html"
-	wp core install --allow-root --admin_user="${WORDPRESS_ADMIN_NAME}" --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --title=${WORDPRESS_TITLE} --url=${WORDPRESS_URL} --path="/var/www/html"
+	gosu wordpress wp config create --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbhost="${WORDPRESS_DB_HOST}" --dbpass="${WORDPRESS_DB_PASSWORD}" --path="/var/www/html"
+	gosu wordpress wp core install --admin_user="${WORDPRESS_ADMIN_NAME}" --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --title=${WORDPRESS_TITLE} --url=${WORDPRESS_URL} --path="/var/www/html"
 fi
-exec "$@" -F -R
+exec gosu wordpress "$@" -F -y /docker/docker-www.conf
