53815f3dd1033c1d5d8b02298ca505e9f1195f5c
marai <masadevs@proton.me>
2024-02-24 08:07:23 +0000

    change wordpress settting

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux
 if [ "$(id -u)" = "0" ]; then
-	chown -R mysql:mysql /etc/mysql/mariadb.conf.d/
 	exec gosu mysql ${BASH_SOURCE[0]} "$@"
 fi
 
@@ -12,11 +11,10 @@ PID=$!
 sleep 3
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'${WORDPRESS_HOST_NAME}' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'${WORDPRESS_HOST_NAME}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
-sed -i -e 's/^bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 kill $!
 wait $!
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eux
 if [ ! -e /var/www/html/wp-config.php ]; then
+	cd /var/www/html
 	cp -fr /var/www/wordpress/* /var/www/html/
-	wp config create --allow-root --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbhost="${WORDPRESS_DB_HOST}" --dbpass="${WORDPRESS_DB_PASSWORD}" --path="/var/www/html" --config-file="/var/www/html/wp-config.php"
+	wp config create --allow-root --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbhost="${WORDPRESS_DB_HOST}" --dbpass="${WORDPRESS_DB_PASSWORD}" --path="/var/www/html"
+	wp core install --allow-root --admin_user="${WORDPRESS_ADMIN_NAME}" --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --title=${WORDPRESS_TITLE} --url=${WORDPRESS_URL} --path="/var/www/html"
 fi
 exec "$@" -F -R
